a3982c0dd77860ae8b7592515387ee1336cd79a9
Ben Pocklington <104888758+benpocklingtonhes@users.noreply.github.com>
2023-10-13 10:20:06 +0200

    fix mix-up of body_index & body_id

--- pykinect_azure/k4abt/frame.py
+++ pykinect_azure/k4abt/frame.py
@@ -8,6 +8,7 @@ from pykinect_azure.k4abt._k4abtTypes import k4abt_body_t, body_colors
 from pykinect_azure.k4a import Image, Capture, Transformation
 from pykinect_azure.k4a._k4atypes import K4A_CALIBRATION_TYPE_DEPTH
 
+
 class Frame:
 	def __init__(self, frame_handle, calibration):
 
@@ -49,9 +50,7 @@ class Frame:
 
 	def get_body_skeleton(self, index=0):
 		skeleton = _k4abt.k4abt_skeleton_t()
-
-		_k4abt.VERIFY(_k4abt.k4abt_frame_get_body_skeleton(self._handle, index, skeleton), "Body tracker get body skeleton failed!")
-
+		_k4abt.VERIFY(_k4abt.k4abt_frame_get_body_skeleton(self._handle,index, skeleton),"Body tracker get body skeleton failed!")
 		return skeleton
 
 	def get_body_id(self, index=0):
@@ -71,20 +70,18 @@ class Frame:
 
 		return bodies
 
-	def get_body(self, bodyIdx = 0):
+	def get_body(self, body_index=0):
 		body_handle = k4abt_body_t()
-		body_handle.id = self.get_body_id(bodyIdx);
-		body_handle.skeleton = self.get_body_skeleton(bodyIdx);
+		body_handle.id = self.get_body_id(body_index)
+		body_handle.skeleton = self.get_body_skeleton(body_index)
 
 		return Body(body_handle)
 
-	def get_body2d(self, bodyIdx = 0, dest_camera = K4A_CALIBRATION_TYPE_DEPTH):
+	def get_body2d(self, body_index=0, dest_camera=K4A_CALIBRATION_TYPE_DEPTH):
+		body_handle = self.get_body(body_index).handle()
+		return Body2d.create(body_handle, self.calibration, body_handle.id, dest_camera)
 
-		body_handle = self.get_body(bodyIdx).handle()
-
-		return Body2d.create(body_handle, self.calibration, bodyIdx, dest_camera)
-
-	def draw_bodies(self, destination_image, dest_camera = K4A_CALIBRATION_TYPE_DEPTH, only_segments = False):
+	def draw_bodies(self, destination_image, dest_camera=K4A_CALIBRATION_TYPE_DEPTH, only_segments=False):
 		num_bodies = self.get_num_bodies()
 
 		for body_id in range(num_bodies):
@@ -92,8 +89,8 @@ class Frame:
 
 		return destination_image
 
-	def draw_body2d(self, destination_image, bodyIdx = 0, dest_camera = K4A_CALIBRATION_TYPE_DEPTH, only_segments = False):
-		return self.get_body2d(bodyIdx, dest_camera).draw(destination_image, only_segments)
+	def draw_body2d(self, destination_image, body_index=0, dest_camera=K4A_CALIBRATION_TYPE_DEPTH, only_segments=False):
+		return self.get_body2d(body_index, dest_camera).draw(destination_image, only_segments)
 
 	def get_device_timestamp_usec(self):
 		return _k4abt.k4abt_frame_get_device_timestamp_usec(self._handle)
@@ -109,7 +106,7 @@ class Frame:
 		return self.transformation.depth_image_to_color_camera_custom(depth_image, self.get_body_index_map())
 
 	def get_transformed_body_index_map_image(self):
-		transformed_body_index_map =self.get_transformed_body_index_map()
+		transformed_body_index_map = self.get_transformed_body_index_map()
 		return transformed_body_index_map.to_numpy()
 
 	def get_segmentation_image(self):
