563367512aac0ae1a13d71ad808388513500bdd8
Ben Pocklington <104888758+benpocklingtonhes@users.noreply.github.com>
2023-10-19 09:44:56 +0200

    fixed some formatting in _k4abtTypes

--- pykinect_azure/k4abt/_k4abtTypes.py
+++ pykinect_azure/k4abt/_k4abtTypes.py
@@ -5,14 +5,14 @@ from pykinect_azure.k4a._k4atypes import k4a_float3_t, k4a_float2_t
 
 # K4A_DECLARE_HANDLE(k4abt_tracker_t);
 class _handle_k4abt_tracker_t(ctypes.Structure):
-	 _fields_= [
+	_fields_ = [
 		("_rsvd", ctypes.c_size_t),
 	]
 k4abt_tracker_t = ctypes.POINTER(_handle_k4abt_tracker_t)
 
 # K4A_DECLARE_HANDLE(k4abt_frame_t);
 class _handle_k4abt_frame_t(ctypes.Structure):
-	 _fields_= [
+	_fields_ = [
 		("_rsvd", ctypes.c_size_t),
 	]
 k4abt_frame_t = ctypes.POINTER(_handle_k4abt_frame_t)
@@ -21,7 +21,7 @@ k4abt_result_t = ctypes.c_int
 K4ABT_RESULT_SUCCEEDED = 0
 K4ABT_RESULT_FAILED = 1
 
-#class k4abt_joint_id_t(CtypeIntEnum):
+# class k4abt_joint_id_t(CtypeIntEnum):
 K4ABT_JOINT_PELVIS = 0
 K4ABT_JOINT_SPINE_NAVEL = 1
 K4ABT_JOINT_SPINE_CHEST = 2
@@ -61,48 +61,48 @@ K4ABT_JOINT_NAMES = ["pelvis", "spine - navel", "spine - chest", "neck", "left c
 					"right wrist", "right hand", "right handtip", "right thumb", "left hip", "left knee", "left ankle", "left foot",
 					"right hip", "right knee", "right ankle", "right foot", "head", "nose", "left eye", "left ear","right eye", "right ear"]
 
-K4ABT_SEGMENT_PAIRS = [[1, 0], 
-					   [2, 1], 
-					   [3, 2], 
-					   [4, 2], 
-					   [5, 4], 
-					   [6, 5], 
-					   [7, 6], 
-					   [8, 7], 
+K4ABT_SEGMENT_PAIRS = [[1, 0],
+					   [2, 1],
+					   [3, 2],
+					   [4, 2],
+					   [5, 4],
+					   [6, 5],
+					   [7, 6],
+					   [8, 7],
 					   [9, 8],
-					   [10, 7], 
-					   [11, 2], 
-					   [12, 11], 
-					   [13, 12], 
-					   [14, 13], 
-					   [15, 14], 
-					   [16, 15], 
-					   [17, 14], 
-					   [18, 0], 
+					   [10, 7],
+					   [11, 2],
+					   [12, 11],
+					   [13, 12],
+					   [14, 13],
+					   [15, 14],
+					   [16, 15],
+					   [17, 14],
+					   [18, 0],
 					   [19, 18],
-					   [20, 19], 
-					   [21, 20], 
-					   [22, 0], 
-					   [23, 22], 
-					   [24, 23], 
-					   [25, 24], 
-					   [26, 3], 
+					   [20, 19],
+					   [21, 20],
+					   [22, 0],
+					   [23, 22],
+					   [24, 23],
+					   [25, 24],
+					   [26, 3],
 					   [27, 26],
-					   [28, 26], 
-					   [29, 26], 
-					   [30, 26], 
+					   [28, 26],
+					   [29, 26],
+					   [30, 26],
 					   [31, 26]]
 
-#class k4abt_sensor_orientation_t(CtypeIntEnum):
+# class k4abt_sensor_orientation_t(CtypeIntEnum):
 K4ABT_SENSOR_ORIENTATION_DEFAULT = 0
 K4ABT_SENSOR_ORIENTATION_CLOCKWISE90 = 1
 K4ABT_SENSOR_ORIENTATION_COUNTERCLOCKWISE90 = 2
 K4ABT_SENSOR_ORIENTATION_FLIP180 = 3
 
-#class k4abt_tracker_processing_mode_t(CtypeIntEnum):
+# class k4abt_tracker_processing_mode_t(CtypeIntEnum):
 K4ABT_TRACKER_PROCESSING_MODE_GPU = 0
 K4ABT_TRACKER_PROCESSING_MODE_CPU = 1
-K4ABT_TRACKER_PROCESSING_MODE_GPU_CUDA = 2     
+K4ABT_TRACKER_PROCESSING_MODE_GPU_CUDA = 2
 K4ABT_TRACKER_PROCESSING_MODE_GPU_TENSORRT = 3
 K4ABT_TRACKER_PROCESSING_MODE_GPU_DIRECTML = 4
 
@@ -148,7 +148,7 @@ class k4a_quaternion_t(ctypes.Union):
 	def __str__(self):
 		return self.wxyz.__str__()
 
-#class k4abt_joint_confidence_level_t(CtypeIntEnum):
+# class k4abt_joint_confidence_level_t(CtypeIntEnum):
 K4ABT_JOINT_CONFIDENCE_NONE = 0
 K4ABT_JOINT_CONFIDENCE_LOW = 1
 K4ABT_JOINT_CONFIDENCE_MEDIUM = 2
@@ -157,7 +157,7 @@ K4ABT_JOINT_CONFIDENCE_LEVELS_COUNT = 4
 
 
 class _k4abt_joint_t(ctypes.Structure):
-	_fields_= [
+	_fields_ = [
 		("position", k4a_float3_t),
 		("orientation", k4a_quaternion_t),
 		("confidence_level", ctypes.c_int),
@@ -170,14 +170,14 @@ class _k4abt_joint_t(ctypes.Structure):
 		self.confidence_level = confidence_level
 
 	def __iter__(self):
-		return {'position':self.position.__iter__(), 
+		return {'position':self.position.__iter__(),
 				'orientation':self.orientation.__iter__(),
 				'confidence_level':self.confidence_level}
 
 k4abt_joint_t = _k4abt_joint_t
 
 class k4abt_skeleton_t(ctypes.Structure):
-	_fields_= [
+	_fields_ = [
 		("joints", _k4abt_joint_t * K4ABT_JOINT_COUNT),
 	]
 
@@ -190,7 +190,7 @@ class k4abt_skeleton_t(ctypes.Structure):
 
 
 class k4abt_body_t(ctypes.Structure):
-	_fields_= [
+	_fields_ = [
 		("id", ctypes.c_uint32),
 		("skeleton", k4abt_skeleton_t),
 	]
@@ -204,7 +204,7 @@ class k4abt_body_t(ctypes.Structure):
 		return {'id':self.id, 'skeleton': self.skeleton.__iter__()}
 
 class _k4abt_joint2D_t(ctypes.Structure):
-	_fields_= [
+	_fields_ = [
 		("position", k4a_float2_t),
 		("confidence_level", ctypes.c_int),
 	]
@@ -215,13 +215,15 @@ class _k4abt_joint2D_t(ctypes.Structure):
 		self.confidence_level = confidence_level
 
 	def __iter__(self):
-		return {'position':self.position.__iter__(),
-				'confidence_level':self.confidence_level}
+		return {
+			'position': self.position.__iter__(),
+			'confidence_level': self.confidence_level
+		}
 
 k4abt_joint2D_t = _k4abt_joint2D_t
 
 class k4abt_skeleton2D_t(ctypes.Structure):
-	_fields_= [
+	_fields_ = [
 		("joints2D", _k4abt_joint2D_t * K4ABT_JOINT_COUNT),
 	]
 
@@ -233,7 +235,7 @@ class k4abt_skeleton2D_t(ctypes.Structure):
 		return {'joints2D': [joint.__iter__() for joint in self.joints2D]}
 
 class k4abt_body2D_t(ctypes.Structure):
-	_fields_= [
+	_fields_ = [
 		("id", ctypes.c_uint32),
 		("skeleton", k4abt_skeleton2D_t),
 	]
@@ -244,7 +246,10 @@ class k4abt_body2D_t(ctypes.Structure):
 		self.skeleton = skeleton
 
 	def __iter__(self):
-		return {'id':self.id, 'skeleton': self.skeleton.__iter__()}
+		return {
+			'id': self.id,
+			'skeleton': self.skeleton.__iter__()
+		}
 
 
 K4ABT_BODY_INDEX_MAP_BACKGROUND = 255
@@ -259,5 +264,13 @@ k4abt_tracker_default_configuration.sensor_orientation = K4ABT_SENSOR_ORIENTATIO
 k4abt_tracker_default_configuration.processing_mode = K4ABT_TRACKER_PROCESSING_MODE_GPU
 k4abt_tracker_default_configuration.gpu_device_id = 0
 
-body_colors = np.ones((256,3), dtype=np.uint8)*K4ABT_BODY_INDEX_MAP_BACKGROUND
-body_colors[:7,:] = np.array([[202, 183, 42], [42, 61, 202], [42, 202, 183], [202, 42,61], [183, 42, 202], [42, 202, 61], [141, 202, 42]]) 
+body_colors = np.ones((256, 3), dtype=np.uint8) * K4ABT_BODY_INDEX_MAP_BACKGROUND
+body_colors[:7, :] = np.array([
+	[202, 183, 42],
+	[42, 61, 202],
+	[42, 202, 183],
+	[202, 42, 61],
+	[183, 42, 202],
+	[42, 202, 61],
+	[141, 202, 42]
+])
